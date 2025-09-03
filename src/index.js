797cf5da26458fa4cb75c363affc57e505f81048
Марина Кодола <109357411+Maryna-ua@users.noreply.github.com>
2025-09-03 12:12:34 +0000

    add basic js and typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello all");
+function recipeGenerator(event) {
+  event.preventDefault();
+  //alert("Generating");
+  let recipeElement = document.querySelector(".answerRecipe");
+  recipeElement.innerHTML = "Recipe for...";
+  new Typewriter(".answerRecipe", {
+    strings: "Recipe for...",
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let recipeRequestElement = document.querySelector("form");
+recipeRequestElement.addEventListener("submit", recipeGenerator);
